00844c473d95eda739fe01aeff8ec01f6e4167ec
Khomsiboy <domkhomsan789@hotmail.com>
2021-06-15 03:33:47 +0200

    Image
    Done with puting images of dogs from Json

--- src/component/Register.jsx
+++ src/component/Register.jsx
@@ -1,48 +1,52 @@
-import React,{ Component } from 'react' 
+import React,{ Component ,  useState } from 'react' 
+import { useEffect } from 'react'
 import {Link} from 'react-router-dom'
+import './register.css'
 
-class Register extends Component{
+const Register = () => {
 
+    const [data, setData] = useState([])
 
-    constructor(props){
-        super(props)
-        this.state = {
-            dogNames: []
-        }
-    }
-
-    componentDidMount(){
+   const GetData= () =>{
         const url = "https://api.jsonbin.io/b/6087d9c3f6655022c46d0b41";
         fetch(url)
         .then(data => data.json())
-        .then(data => this.setState({dogNames: data.dogs}))
+        .then(data => {
+           setData(data)
+        })
     }
 
-    render() {
-        const { dogNames } = this.state;
+    useEffect(() =>{
+      GetData()
+    },[])
+
+    return( 
+       
+        <section class="container">
+           
+         
+       
+             {data.map(dogs => 
+               
+             
+                  <div class="card">
+                      <div><img className="card-image" src={dogs.img} alt="dog"/></div>
+                      <h2>{dogs.name}</h2>
+                      <p>Breed:{dogs.breed}<br/>Age:  {dogs.age}</p>
+
+                      <a href="">SHOW</a>
+                  </div>
+             
+         )}
+       
         
-        return(
-        <div>
-        <div>
-        <h1 >Doggy Daycare </h1>
-        </div>
-        <div >
-        {dogNames.map((user) => (
-        <Link key={user.chipNumber}>
-        <img src={user.img} alt={`${user.name} img`}/>
-        <div >
-        <h5 >{user.name} </h5>
-        <Link to={"/Dogs/"+user.chipNumber}>More about this friend</Link> 
-        </div>
-        </Link>
-        
-        ))}
-        </div>
-        </div>
-    )
+       </section>
+     
+
+    );
 }
 
-}
+
 
 
 
