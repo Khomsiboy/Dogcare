be65b2ef139e70b2562d1abb4b9974cfa04e8614
Khomsiboy <domkhomsan789@hotmail.com>
2021-06-18 16:23:12 +0200

    added hash router
    added hash router

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import logo from './logo.svg';
 import './App.css';
-import {BrowserRouter as Router, Route,Switch} from 'react-router-dom'
+import { Route, HashRouter as Router, Link, Switch } from "react-router-dom"
 import Welcome from './component/welcome';
 import Register from './component/Register';
 import Info from './component/Info';
